c2ff284ef39938b8178e380950449e094510a7b1
VictoriaKuzminova <119403787+VictoriaKuzminova@users.noreply.github.com>
2022-12-01 21:34:23 +0300

    Add files via upload

---
+++ Графическое приложение 3_3/3/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace _3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(27, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(287, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Начальная точка диапазона : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(27, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(277, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Конечная точка диапазона : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(27, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Шаг: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(361, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 30);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(361, 76);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(157, 30);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(112, 134);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(157, 30);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(361, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 28);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Вывести";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(358, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(165, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Перегруженный метод: ";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(32, 208);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(291, 230);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(361, 208);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(291, 230);
+            this.richTextBox2.TabIndex = 9;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 450);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Перегрузка";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ Графическое приложение 3_3/3/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3
+{
+    public partial class Form1 : Form
+    {
+        private double a;
+        private double b;
+        private double h;
+
+        private double f(double x)
+        {
+            double y = 0;
+            if (Math.Pow(x, 2) + 2 * x + 1 < 2)
+                y = Math.Pow(x, 2);
+            else if (Math.Pow(x, 2) + 2 * x + 1 >= 3)
+                y = 0.0;
+            else
+                y = (1 / (Math.Pow(x, 2) - 1));
+            return y;
+        }
+
+        private void f(double x, out double y)
+        {
+            if (Math.Pow(x, 2) + 2 * x + 1 < 2)
+                y = Math.Pow(x, 2);
+            else if (Math.Pow(x, 2) + 2 * x + 1 >= 3)
+                y = 0.0;
+            else
+                y = (1 / (Math.Pow(x, 2) - 1));
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "")
+            {
+                MessageBox.Show("Введены не все значения!!!");
+            }
+            else
+            {
+                richTextBox1.Text = "";
+                richTextBox2.Text = "";
+                try
+                {
+                    a = double.Parse(textBox1.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Некорректный ввод!");
+                    return;
+                }
+                try
+                {
+                    b = double.Parse(textBox2.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Некорректный ввод!");
+                    return;
+                }
+                if (a > b)
+                {
+                    MessageBox.Show("Начальная точка диапазона не может быть больше конечной точки");
+                    return;
+                }
+                try
+                {
+                    h = double.Parse(textBox3.Text);
+                    if (h == 0)
+                    {
+                        MessageBox.Show("Шаг не может быть равен 0");
+                        return;
+                    }
+                    else if (h < 0)
+                    {
+                        MessageBox.Show("Шаг не может быть меньше 0");
+                        return;
+
+                    }
+                }
+                catch
+                {
+                    Console.Write("Некорректный ввод!");
+                }
+                for (double i = a; i <= b; i += h)
+                {
+                    richTextBox1.Text += "f(" + Math.Round(i, 2) + ") = " + Math.Round(f(i), 2) + "\n";
+                }
+                for (double i = a; i <= b; i += h)
+                {
+                    f(i, out double y);
+                    richTextBox2.Text += "f(" + Math.Round(i, 2) + ") = " + Math.Round(y, 2) + "\n";
+                }
+            }
+        }
+    }
+}
